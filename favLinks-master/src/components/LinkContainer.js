b739e14d68f9c28cc5385ea63685df34e0134857
Ganesh Jainarain <RicheyJay2@gmail.com>
2020-10-08 17:34:58 -0400

    Completed Step 4, 'Render your Table component in LinkContainer.js'

--- favLinks-master/src/components/LinkContainer.js
+++ favLinks-master/src/components/LinkContainer.js
@@ -27,6 +27,7 @@ class LinkContainer extends React.Component {
                 <h1>My Favorite Links</h1>
                 <p>Add a new url with a name and link to the table.</p>
                 {/*TODO - Add Table Component */}
+                <Table/>
                 
                 <br/>
 
